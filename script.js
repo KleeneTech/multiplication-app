eee560fc152f63fcf4eb27a8e2195b1b57dbc409
KleeneTech <diala.kenneth98@gmail.com>
2023-11-19 00:11:06 -0800

    Added score to localstorage

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 // Random numbers
-const randomNumber1 = Math.ceil(Math.random() * 20);
-const randomNumber2 = Math.ceil(Math.random() * 20);
+const randomNumber1 = Math.ceil(Math.random() * 10);
+const randomNumber2 = Math.ceil(Math.random() * 10);
 
 // Question
 const questionEl = document.getElementById("question");
+const formEl = document.getElementById("form");
+
+let score = JSON.parse(localStorage.getItem("score"));
 
 questionEl.innerText = `What is ${randomNumber1} multiplied by ${randomNumber2}?`;
+const input = document.getElementById("input");
+
+// Correct Answer
+const correctAnswer = randomNumber1 * randomNumber2;
+
+formEl.addEventListener("submit", () => {
+  const userAnswer = +input.value;
+  if (userAnswer === correctAnswer) {
+    score++;
+    updateLocalStorage();
+  } else {
+    score--;
+    updateLocalStorage();
+  }
+});
+
+function updateLocalStorage() {
+  localStorage.setItem("score", JSON.stringify(score));
+}
