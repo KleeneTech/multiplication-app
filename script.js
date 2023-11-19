19a8ff201e7d1c6e34ac17b6ec56659a0a85c862
KleeneTech <diala.kenneth98@gmail.com>
2023-11-19 00:24:45 -0800

    Updated the score section

--- script.js
+++ script.js
@@ -2,14 +2,21 @@
 const randomNumber1 = Math.ceil(Math.random() * 10);
 const randomNumber2 = Math.ceil(Math.random() * 10);
 
-// Question
+// Global Elements
 const questionEl = document.getElementById("question");
 const formEl = document.getElementById("form");
+const input = document.getElementById("input");
+const scoreEl = document.getElementById("score");
 
 let score = JSON.parse(localStorage.getItem("score"));
 
+if (!score) {
+  score = 0;
+}
+
+scoreEl.innerText = `score: ${score}`;
+
 questionEl.innerText = `What is ${randomNumber1} multiplied by ${randomNumber2}?`;
-const input = document.getElementById("input");
 
 // Correct Answer
 const correctAnswer = randomNumber1 * randomNumber2;
